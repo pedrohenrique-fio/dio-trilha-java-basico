7eca2d41b6e55cdb7644ca8d5c1f178a8170c1fb
Pedro Henrique <pedrohenriquefiorimguedes@gmail.com>
2025-03-10 20:18:22 -0300

    Realização do código no arquivo "ContaTerminal".

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,7 +1,32 @@
-public class ContaTerminal {
+import java.util.Scanner;
+import java.util.Locale;
 
-    public static void main (String[] args) throws Exception {
-        System.out.println("Hello, World!");
+public class ContaTerminal {
+    public static void main (String[] args) {
+
+        /* Apresentação do sistema */
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        System.out.println("------ Seja bem vindo(a) ao sistema de criação de agências proposto pelo desafio da DigitalInnovationOne! ------");
+
+        /* Inserção do nome do cliente*/
+        System.out.println("Insira o nome do cliente que estará criando a agencia abaixo:");
+        String nomeCliente = scanner.nextLine();
+
+         /* Inserção do nome da agencia */
+        System.out.println("Insira o nome da agência abaixo:");
+        String nomeAgencia = scanner.nextLine();
+
+         /* Inserção do número da agencia */
+        System.out.println("Insira o número da agência:");
+        int numeroAgencia = scanner.nextInt();
+
+         /* inserção do saldo da agencia */
+        System.out.println("Insira o saldo da agência:");
+        double saldoAgencia = scanner.nextDouble(); 
+
+        /* Apresentação dos dados alocados pelo usuário e finalização do sistema */
+        System.out.println("Olá " + nomeCliente + " " + "Obrigado por criar uma conta em nosso sistema, sua agência é " +nomeAgencia+ ", " + "conta " + numeroAgencia + " " + "e seu saldo de " + saldoAgencia + " " + "já está disponivel para saque");
+
     }
     
 }
